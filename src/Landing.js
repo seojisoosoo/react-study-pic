7cf9156d3bbd7743c94321c8184388fc94e8cf89
Seojisoo20191941 <amy0480@naver.com>
2022-11-02 15:19:13 +0900

    [style] 랜딩 애니메이션

--- src/Landing.js
+++ src/Landing.js
@@ -2,6 +2,10 @@ import React from "react";
 import { useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 // import { Link } from "react-router-dom";
+import Animation from "./Animation";
+import styled from "styled-components";
+import "./Landing.css";
+import "animate.css";
 
 const Landing = () => {
   const nameRef = useRef(null);
@@ -16,21 +20,36 @@ const Landing = () => {
     console.log(name);
     setHi("님 반갑습니다");
   };
+
   return (
-    <div>
-      <p>미술관에 온 것을 환영합니다!!!!!!!!!!!!</p>
+    <Dom>
+      <Font className="animate__animated animate__tada">
+        미술관에 온 것을 환영합니다!!!!!!!!!!!!
+      </Font>
       <input placeholder="이름을 입력하세요" ref={nameRef} />
       {/* <button onClick={() => saveClick("/visitors", nameRef.current.value)}> */}
       <button onClick={nameSave}>저장</button>
-      <p>
+      <Font className="animate_strech">
         {name}
         {hi}
-      </p>
+      </Font>
+      <Animation />
       {/* <Link to="/visitors">
         <button>방명록 쓰기</button>
       </Link> */}
-    </div>
+    </Dom>
   );
 };
 
 export default Landing;
+
+const Dom = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+const Font = styled.h1`
+  color: white;
+  margin: 30px 0px;
+  text-align: center;
+`;

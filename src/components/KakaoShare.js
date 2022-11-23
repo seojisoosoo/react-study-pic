b75f5b50dd12eba98be49d6f11fb2140dd600b44
Seojisoo20191941 <amy0480@naver.com>
2022-11-23 20:33:25 +0900

    fix : 카카오에러 해결중

--- src/components/KakaoShare.js
+++ src/components/KakaoShare.js
@@ -1,67 +1,67 @@
-// import React, { useEffect } from 'react'
-// const KAKAO_KEY = process.env.REACT_APP_KAKAO_KEY
+import React, { useEffect } from 'react'
+const KAKAO_KEY = process.env.REACT_APP_KAKAO_KEY
 
 const KakaoShare = () => {
-  // window.Kakao.init(KAKAO_KEY)
-  // const kakao = window.Kakao
-  // console.log(kakao.isInitialized)
-  // console.log(window.Kakao)
-  // useEffect(() => {
-  //   createKakaoButton()
-  // }, [])
-  // const createKakaoButton = () => {
-  //   const KAKAO_KEY = process.env.REACT_APP_KAKAO_KEY
-  //   // kakao sdk script이 정상적으로 불러와졌으면 window.Kakao로 접근이 가능합니다
-  //   if (window.Kakao) {
-  //     const kakao = window.Kakao
-  //     // 중복 initialization 방지
-  //     if (!kakao.isInitialized()) {
-  //       // 두번째 step 에서 가져온 javascript key 를 이용하여 initialize
-  //       kakao.init(KAKAO_KEY)
-  //       console.log(kakao.isInitialized)
-  //       console.log(window.Kakao)
-  //     }
-  //     kakao.Share.createDefaultButton({
-  //       // Render 부분 id=kakao-link-btn 을 찾아 그부분에 렌더링을 합니다
-  //       container: '#kakao-link-btn',
-  //       objectType: 'feed',
-  //       content: {
-  //         title: '이중섭 미술관',
-  //         description: '환영해요',
-  //         imageUrl: 'IMAGE_URL', // i.e. process.env.FETCH_URL + '/logo.png'
-  //         link: {
-  //           mobileWebUrl: window.location.href,
-  //           webUrl: window.location.href,
-  //         },
-  //       },
-  //       social: {
-  //         likeCount: 77,
-  //         commentCount: 55,
-  //         sharedCount: 333,
-  //       },
-  //       buttons: [
-  //         {
-  //           title: '웹으로 보기',
-  //           link: {
-  //             mobileWebUrl: window.location.href,
-  //             webUrl: window.location.href,
-  //           },
-  //         },
-  //         {
-  //           title: '앱으로 보기',
-  //           link: {
-  //             mobileWebUrl: window.location.href,
-  //             webUrl: window.location.href,
-  //           },
-  //         },
-  //       ],
-  //     })
-  //   }
-  // }
+  window.Kakao.init(KAKAO_KEY)
+  const kakao = window.Kakao
+  console.log(kakao.isInitialized)
+  console.log(window.Kakao)
+  useEffect(() => {
+    createKakaoButton()
+  }, [])
+  const createKakaoButton = () => {
+    const KAKAO_KEY = process.env.REACT_APP_KAKAO_KEY
+    // kakao sdk script이 정상적으로 불러와졌으면 window.Kakao로 접근이 가능합니다
+    if (window.Kakao) {
+      const kakao = window.Kakao
+      // 중복 initialization 방지
+      if (!kakao.isInitialized()) {
+        // 두번째 step 에서 가져온 javascript key 를 이용하여 initialize
+        kakao.init(KAKAO_KEY)
+        console.log(kakao.isInitialized)
+        console.log(window.Kakao)
+      }
+      kakao.Share.createDefaultButton({
+        // Render 부분 id=kakao-link-btn 을 찾아 그부분에 렌더링을 합니다
+        container: '#kakao-link-btn',
+        objectType: 'feed',
+        content: {
+          title: '이중섭 미술관',
+          description: '환영해요',
+          imageUrl: 'IMAGE_URL', // i.e. process.env.FETCH_URL + '/logo.png'
+          link: {
+            mobileWebUrl: window.location.href,
+            webUrl: window.location.href,
+          },
+        },
+        social: {
+          likeCount: 77,
+          commentCount: 55,
+          sharedCount: 333,
+        },
+        buttons: [
+          {
+            title: '웹으로 보기',
+            link: {
+              mobileWebUrl: window.location.href,
+              webUrl: window.location.href,
+            },
+          },
+          {
+            title: '앱으로 보기',
+            link: {
+              mobileWebUrl: window.location.href,
+              webUrl: window.location.href,
+            },
+          },
+        ],
+      })
+    }
+  }
   return (
     <div className="kakao-share-button">
       {/* Kakao share buttonㅇ */}
-      <button id="kakao-link-btn" onClick={()=>{console.log(process.env.REACT_APP_AMUMAL)}}>
+      <button id="kakao-link-btn">
         카톡 공유
       </button>
     </div>

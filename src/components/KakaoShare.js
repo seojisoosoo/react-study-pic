df21134b8dbde831412898f2b24f9391cc85266f
Seojisoo20191941 <amy0480@naver.com>
2022-11-23 20:20:44 +0900

    fix : undefined 에러 해결중

--- src/components/KakaoShare.js
+++ src/components/KakaoShare.js
@@ -14,7 +14,7 @@ const KakaoShare = () => {
     // kakao sdk script이 정상적으로 불러와졌으면 window.Kakao로 접근이 가능합니다
     if (window.Kakao) {
       const kakao = window.Kakao
-      // 중복 initialization 방지ㅇ
+      // 중복 initialization 방지
       if (!kakao.isInitialized()) {
         // 두번째 step 에서 가져온 javascript key 를 이용하여 initialize
         kakao.init(KAKAO_KEY)

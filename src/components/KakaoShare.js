17eaaafa3f616460042119eecdc0bf856695080e
Seojisoo20191941 <amy0480@naver.com>
2022-11-23 20:37:10 +0900

    fix : 카카오에러 해결중

--- src/components/KakaoShare.js
+++ src/components/KakaoShare.js
@@ -1,26 +1,26 @@
 import React, { useEffect } from 'react'
-const KAKAO_KEY = process.env.REACT_APP_KAKAO_KEY
+// const KAKAO_KEY = process.env.REACT_APP_KAKAO_KEY
 
 const KakaoShare = () => {
-  window.Kakao.init(KAKAO_KEY)
-  const kakao = window.Kakao
-  console.log(kakao.isInitialized)
-  console.log(window.Kakao)
+  // window.Kakao.init(KAKAO_KEY)
+  // const kakao = window.Kakao
+  // console.log(kakao.isInitialized)
+  // console.log(window.Kakao)
   useEffect(() => {
     createKakaoButton()
   }, [])
   const createKakaoButton = () => {
-    const KAKAO_KEY = process.env.REACT_APP_KAKAO_KEY
+    // const KAKAO_KEY = process.env.REACT_APP_KAKAO_KEY
     // kakao sdk script이 정상적으로 불러와졌으면 window.Kakao로 접근이 가능합니다
     if (window.Kakao) {
       const kakao = window.Kakao
       // 중복 initialization 방지
-      if (!kakao.isInitialized()) {
-        // 두번째 step 에서 가져온 javascript key 를 이용하여 initialize
-        kakao.init(KAKAO_KEY)
-        console.log(kakao.isInitialized)
-        console.log(window.Kakao)
-      }
+      // if (!kakao.isInitialized()) {
+      //   // 두번째 step 에서 가져온 javascript key 를 이용하여 initialize
+      //   kakao.init(KAKAO_KEY)
+      //   console.log(kakao.isInitialized)
+      //   console.log(window.Kakao)
+      // }
       kakao.Share.createDefaultButton({
         // Render 부분 id=kakao-link-btn 을 찾아 그부분에 렌더링을 합니다
         container: '#kakao-link-btn',

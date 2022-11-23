0558010b531df5b4604c7d1220e638cfe5ac70d6
Seojisoo20191941 <amy0480@naver.com>
2022-11-23 20:39:07 +0900

    fix : 카카오에러 해결중

--- src/components/KakaoShare.js
+++ src/components/KakaoShare.js
@@ -10,17 +10,17 @@ const KakaoShare = () => {
     createKakaoButton()
   }, [])
   const createKakaoButton = () => {
-    // const KAKAO_KEY = process.env.REACT_APP_KAKAO_KEY
+    const KAKAO_KEY = process.env.REACT_APP_KAKAO_KEY
     // kakao sdk script이 정상적으로 불러와졌으면 window.Kakao로 접근이 가능합니다
     if (window.Kakao) {
       const kakao = window.Kakao
       // 중복 initialization 방지
-      // if (!kakao.isInitialized()) {
-      //   // 두번째 step 에서 가져온 javascript key 를 이용하여 initialize
-      //   kakao.init(KAKAO_KEY)
-      //   console.log(kakao.isInitialized)
-      //   console.log(window.Kakao)
-      // }
+      if (!kakao.isInitialized()) {
+        // 두번째 step 에서 가져온 javascript key 를 이용하여 initialize
+        kakao.init(KAKAO_KEY)
+        console.log(kakao.isInitialized)
+        console.log(window.Kakao)
+      }
       kakao.Share.createDefaultButton({
         // Render 부분 id=kakao-link-btn 을 찾아 그부분에 렌더링을 합니다
         container: '#kakao-link-btn',
